eef6f5176ee101f6654a354155b0f8610f628103
usman00111 <G00400065@atu.ie>
2024-10-04 14:30:07 +0000

    import in app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-
-import './App.css';
-import Header from './Components/Header';
-import Content from './Components/Content';
-import Footer from './Components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './Components/NavigationBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// imported the components 4 .js files 
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Content from './Components/Content';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
-      <Footer />
+    <Router>
       <NavigationBar />
-    </div>
-  )
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
