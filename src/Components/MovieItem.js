1e154f61b24b524932f68fa0f630a22c16a88eca
usman00111 <G00400065@atu.ie>
2024-10-11 14:44:07 +0000

    task 3 complete

--- src/Components/MovieItem.js
+++ src/Components/MovieItem.js
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
+import Card from 'react-bootstrap/Card';
+
 //constant create returning h3 tag
 const MovieItem = (props)=>{
-    return (
+    useEffect(() => {
+        console.log("Movie Item:", props.mymovie);
+      }, [props.mymovie]); // Only run this effect when the mymovie prop changes
+    
+      return (
         <div>
-            <h3>Hello from the Movie MovieItem</h3>
+          <Card>
+            <Card.Header>{props.mymovie.Title}</Card.Header>
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <img src={props.mymovie.Poster} alt={props.mymovie.Title} />
+                <footer>{props.mymovie.Year}</footer>
+              </blockquote>
+            </Card.Body>
+          </Card>
         </div>
-    )
+      );
 }
 // this allows code to be imported by app.js
 export default MovieItem;
--- src/Components/movies.js
+++ src/Components/movies.js
@@ -2,14 +2,12 @@ import MovieItem from "./MovieItem";
 
 //constant called movies with argument called props
 const Movies = (props )=>{
-    return ( // returning line and props. myMovie variable name from rea.js
-        <div>
-            Hello from movies component
-            {console.log(props.myMovies)}
-            <MovieItem /> 
-            {/* embeding movie item into movies */}
-        </div>
-    )
-}
+        return props.myMovies.map(
+            (movie) => {
+                return <MovieItem mymovie={movie} key={movie.imbdID} />
+            }
+        );
+    }
+
 
 export default Movies;
